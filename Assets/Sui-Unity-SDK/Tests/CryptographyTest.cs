a6776700d0b3d0ecada95fa1265aae49e33a26e4
Irvin <irvsteve@gmail.com>
2023-08-25 23:30:38 -0400

    Set up unit test framework

---
+++ Assets/Sui-Unity-SDK/Tests/CryptographyTest.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using System;
+using System.Text;
+
+namespace Sui.Cryptography
+{
+    public class CryptographyTest
+    {
+        [Test]
+        public void PrivateKeyFromBytesSuccess()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
