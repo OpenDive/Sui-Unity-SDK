8c0cf854e87d1d9544dcaa1099701e4624636a7b
Irvin <irvsteve@gmail.com>
2023-08-26 22:03:55 -0400

    Add Ed25519 Signature implementation

--- Assets/Sui-Unity-SDK/Code/Sui.Cryptography/ED25519/Signature.cs
+++ Assets/Sui-Unity-SDK/Code/Sui.Cryptography/ED25519/Signature.cs
@@ -1,12 +1,46 @@
-using System;
+using Chaos.NaCl;
 
 namespace Sui.Cryptography.Ed25519
 {
     public class Signature : ISignature
     {
+        public const int SignatureLength = 64;
+
+        private readonly byte[] _signatureBytes;
+
+        private string _signature;
+
         public Signature(byte[] signature)
         {
-            throw new NotImplementedException();
+            _signatureBytes = signature;
+        }
+
+        public byte[] Data()
+        {
+            return _signatureBytes;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            string signatureHex = CryptoBytes.ToHexStringLower(_signatureBytes);
+            _signature = "0x" + signatureHex;
+
+            return _signature;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Signature signature)
+            {
+                return signature.ToString() == this.ToString();
+            }
+
+            return false;
         }
     }
 }
