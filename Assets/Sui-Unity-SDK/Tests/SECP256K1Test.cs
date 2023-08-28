85feadc6128d0f0f62fb399631a3d78901327f7d
Irvin <irvsteve@gmail.com>
2023-08-28 05:24:22 -0400

    Add namespace to SECP256K1 test

--- Assets/Sui-Unity-SDK/Tests/SECP256K1Test.cs
+++ Assets/Sui-Unity-SDK/Tests/SECP256K1Test.cs
@@ -1,76 +1,80 @@
 using NUnit.Framework;
 
-public class SECP256K1Test
+namespace Sui.Tests.Cryptography
 {
-    [Test]
-    public void PrivateKeyFromBytesSuccess()
+    public class SECP256K1Test
     {
-        Assert.AreEqual(1, 0);
+        [Test]
+        public void PrivateKeyFromBytesSuccess()
+        {
+            Assert.AreEqual(1, 0);
+        }
+
+        [Test]
+        public void PrivateKeyFromBytesInvalidLength()
+        {
+            Assert.AreEqual(1, 0);
+        }
+
+        [Test]
+        public void PrivateKeyFromHexStringSuccess()
+        {
+            Assert.AreEqual(1, 0);
+        }
+
+        [Test]
+        public void PrivateKeyFromHexStringInvalid()
+        {
+            Assert.AreEqual(1, 0);
+        }
+
+        [Test]
+        public void PrivateKeyFromBase58StringSuccess()
+        {
+            Assert.AreEqual(1, 0);
+        }
+
+        [Test]
+        public void PrivateKeyFromBase58StringInvalid()
+        {
+            Assert.AreEqual(1, 0);
+        }
+
+        [Test]
+        public void PrivateKeyToString()
+        {
+            Assert.AreEqual(1, 0);
+        }
+
+        [Test]
+        public void PrivateKeyToHexString()
+        {
+            Assert.AreEqual(1, 0);
+        }
+
+        [Test]
+        public void PrivateKeyToBase58String()
+        {
+            Assert.AreEqual(1, 0);
+        }
+
+        [Test]
+        public void PrivateKeyComparisonTrue()
+        {
+            Assert.AreEqual(1, 0);
+        }
+
+        [Test]
+        public void PrivateKeyComparisonFalse()
+        {
+            Assert.AreEqual(1, 0);
+        }
+
+        [Test]
+        public void PrivateKeySignature()
+        {
+            Assert.AreEqual(1, 0);
+        }
     }
 
-    [Test]
-    public void PrivateKeyFromBytesInvalidLength()
-    {
-        Assert.AreEqual(1, 0);
-    }
-
-    [Test]
-    public void PrivateKeyFromHexStringSuccess()
-    {
-        Assert.AreEqual(1, 0);
-    }
-
-    [Test]
-    public void PrivateKeyFromHexStringInvalid()
-    {
-        Assert.AreEqual(1, 0);
-    }
-
-    [Test]
-    public void PrivateKeyFromBase58StringSuccess()
-    {
-        Assert.AreEqual(1, 0);
-    }
-
-    [Test]
-    public void PrivateKeyFromBase58StringInvalid()
-    {
-        Assert.AreEqual(1, 0);
-    }
-
-    [Test]
-    public void PrivateKeyToString()
-    {
-        Assert.AreEqual(1, 0);
-    }
-
-    [Test]
-    public void PrivateKeyToHexString()
-    {
-        Assert.AreEqual(1, 0);
-    }
-
-    [Test]
-    public void PrivateKeyToBase58String()
-    {
-        Assert.AreEqual(1, 0);
-    }
-
-    [Test]
-    public void PrivateKeyComparisonTrue()
-    {
-        Assert.AreEqual(1, 0);
-    }
-
-    [Test]
-    public void PrivateKeyComparisonFalse()
-    {
-        Assert.AreEqual(1, 0);
-    }
-
-    [Test]
-    public void PrivateKeySignature()
-    {
-        Assert.AreEqual(1, 0);
-    }
 }
