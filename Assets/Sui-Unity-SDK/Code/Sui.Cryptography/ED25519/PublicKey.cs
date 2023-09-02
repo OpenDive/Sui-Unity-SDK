1d9cbab11480ac5c0aef19c86be11c40953533fd
Irvin <irvsteve@gmail.com>
2023-09-02 17:50:57 -0400

    Declare ED25519 public key class

---
+++ Assets/Sui-Unity-SDK/Code/Sui.Cryptography/ED25519/PublicKey.cs
@@ -0,0 +1,9 @@
+namespace Sui.Cryptography.Ed25519
+{
+    /// <summary>
+    /// Implements an ED25519 public key functionality 
+    /// </summary>
+    public class PublicKey : PublicKeyBase
+    {
+    }
+}
