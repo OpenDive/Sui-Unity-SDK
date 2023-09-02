dbb519e91961724eed556ad94767bd1089e7e9c6
Irvin <irvsteve@gmail.com>
2023-09-02 02:16:22 -0400

    Add Signature interface

--- Assets/Sui-Unity-SDK/Code/Sui.Cryptography/ISignature.cs
+++ Assets/Sui-Unity-SDK/Code/Sui.Cryptography/ISignature.cs
@@ -1,6 +1,22 @@
+using System.Collections.Generic;
+
 namespace Sui.Cryptography
 {
-    public class ISignature
+    public interface ISignature
     {
+        public string Serialize();
+        public ISignature Deserialize(string serializedSignature);
+
+        public static string KeyHex
+        {
+            get
+            {
+                return "";
+            }
+
+            set
+            {
+            }
+        }
     }
 }
