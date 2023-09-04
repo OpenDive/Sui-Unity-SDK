2ac9ea0f28b75dd518550db6dd3be875df7cc559
Irvin <irvsteve@gmail.com>
2023-09-03 20:05:44 -0400

    Update ED25519 documentation

--- Assets/Sui-Unity-SDK/Code/Sui.Cryptography/ED25519/PublicKey.cs
+++ Assets/Sui-Unity-SDK/Code/Sui.Cryptography/ED25519/PublicKey.cs
@@ -7,10 +7,27 @@ namespace Sui.Cryptography.Ed25519
     /// </summary>
     public class PublicKey : PublicKeyBase
     {
+        /// <summary>
+        /// Defines signature scheme to be ED25519
+        /// </summary>
         public override SignatureScheme SignatureScheme { get => SignatureScheme.ED25519; }
-        public override int KeyLength { get => 32; }
 
+        /// <summary>
+        /// The size of an ED25519 public key - 32
+        /// </summary>
+        public override int KeyLength { get => SignatureSchemeToSize.ED25519; }
+
+        /// <summary>
+        /// Creates an ED25519 PublicKey object from a byte array that represents a public key
+        /// </summary>
+        /// <param name="publicKey">ED25519 public key as a byte array buffer</param>
         public PublicKey(byte[] publicKey) : base(publicKey) { }
+
+        /// <summary>
+        /// Creates an ED25519 PublicKey object from a base64 or hex string that represents a public key
+        /// </summary>
+        /// <param name="publicKey">A hex or base64 string that represents a ED25519 public key</param>
+        public PublicKey(string publicKey) : base(publicKey) { }
 
         /// <summary>
         /// Return the Sui address associated with this Ed25519 public key
@@ -20,7 +37,7 @@ namespace Sui.Cryptography.Ed25519
 
         public override bool Verify(byte[] message, SignatureBase signature)
         {
-            throw new System.NotImplementedException();
+            return Chaos.NaCl.Ed25519.Verify(signature.Data(), message, ToRawBytes());
         }
     }
 }
