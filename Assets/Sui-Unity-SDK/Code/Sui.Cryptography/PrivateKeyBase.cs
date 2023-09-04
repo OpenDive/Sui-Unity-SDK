294a04ef5613f3b25c8f1baba6cd97f7de0622e6
Irvin <irvsteve@gmail.com>
2023-09-04 18:14:01 -0400

    Update comments

--- Assets/Sui-Unity-SDK/Code/Sui.Cryptography/PrivateKeyBase.cs
+++ Assets/Sui-Unity-SDK/Code/Sui.Cryptography/PrivateKeyBase.cs
@@ -5,44 +5,49 @@ using static Sui.Cryptography.SignatureUtils;
 
 namespace Sui.Cryptography
 {
+    /// <summary>
+    /// Represents all the properties and basic functions of a private key.
+    /// </summary>
     public abstract class PrivateKeyBase
     {
         /// <summary>
-        /// Signature scheme for the private key
+        /// Signature scheme for the private key.
         /// </summary>
         public abstract SignatureScheme SignatureScheme { get; }
 
         /// <summary>
-        /// The length of the private key
+        /// The length of the private key.
         /// </summary>
         public const int KeyLength = 32;
 
         /// <summary>
-        /// The length of the extended private key
+        /// The length of the extended private key.
         /// </summary>
         public const int ExtendedKeyLength = 64;
 
         /// <summary>
-        /// Extended private key as a byte array
+        /// Extended private key as a byte array.
         /// </summary>
         protected byte[] _extendedKeyBytes;
 
         /// <summary>
-        /// Private key represented as a byte array
+        /// Private key represented as a byte array.
         /// </summary>
         protected byte[] _keyBytes;
 
         /// <summary>
-        /// Public key represented as a hex string
+        /// Public key represented as a hex string.
         /// </summary>
         protected string _keyHex;
 
         /// <summary>
-        /// Public key represented as a base64 string
+        /// Public key represented as a base64 string.
         /// </summary>
         protected string _keyBase64;
 
-
+        /// <summary>
+        /// Returns the private key as a hex string.
+        /// </summary>
         public string KeyHex
         {
             get
@@ -69,6 +74,9 @@ namespace Sui.Cryptography
             set => _keyHex = value;
         }
 
+        /// <summary>
+        /// Returns the private key as a base64 string.
+        /// </summary>
         public string KeyBase64
         {
             get
@@ -91,18 +99,21 @@ namespace Sui.Cryptography
             set => _keyBase64 = value;
         }
 
+        /// <summary>
+        /// Returns the private key as a byte array.
+        /// </summary>
         public abstract byte[] KeyBytes { get; set; }
 
         /// <summary>
-        /// Return the private key as a hex string
+        /// Return the private key as a hex string.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The private key as a hex string.</returns>
         public string ToHex() => KeyHex;
 
         /// <summary>
-        /// Return the private key as a base64 string
+        /// Return the private key as a base64 string.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The private key as a base64 string.</returns>
         public string ToBase64() => KeyBase64;
         //public void Serialize(Serializer serializer);
     }
