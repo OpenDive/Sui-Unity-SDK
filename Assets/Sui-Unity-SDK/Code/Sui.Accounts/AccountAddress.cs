5b32d44a285ce97644f48c8e5c4d8f23d13ae7b8
Irvin <irvsteve@gmail.com>
2023-09-05 03:06:01 -0400

    Update AccountAddress implementation

--- Assets/Sui-Unity-SDK/Code/Sui.Accounts/AccountAddress.cs
+++ Assets/Sui-Unity-SDK/Code/Sui.Accounts/AccountAddress.cs
@@ -1,8 +1,21 @@
+using System;
+
 namespace Sui.Accounts
 {
     public class AccountAddress
     {
         private static readonly int Length = 32;
         private readonly byte[] AddressBytes;
+
+        /// <summary>
+        /// Create an AccountAddress object from a byte array representation.
+        /// </summary>
+        /// <param name="address"></param>
+        public AccountAddress(byte[] address)
+        {
+            if (address.Length != Length)
+                throw new ArgumentException("Invalid address length. It must be " + Length + " bytes");
+            AddressBytes = address;
+        }
     }
 }
