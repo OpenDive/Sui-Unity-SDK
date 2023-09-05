24760214276a1d509b3fc7ad56c2edc37a95c933
Irvin <irvsteve@gmail.com>
2023-09-05 02:28:44 -0400

    Update PrivateKey tests

--- Assets/Sui-Unity-SDK/Tests/ED25519Test.cs
+++ Assets/Sui-Unity-SDK/Tests/ED25519Test.cs
@@ -45,13 +45,21 @@ namespace Sui.Tests.Cryptography
         [Test]
         public void PrivateKeyFromHexStringSuccess()
         {
-            Assert.AreEqual(1, 0);
+            string pkHex = "0x99da9559e15e913ee9ab2e53e3dfad575da33b49be1125bb922e33494f498828";
+            PrivateKey pk1 = new PrivateKey(pkHex);
+
+            string expectedPkBase64 = "mdqVWeFekT7pqy5T49+tV12jO0m+ESW7ki4zSU9JiCg=";
+
+            Assert.AreEqual(32, pk1.KeyBytes.Length);
+            Assert.AreEqual(expectedPkBase64, pk1.KeyBase64);
         }
 
         [Test]
         public void PrivateKeyFromHexStringInvalid()
         {
-            Assert.AreEqual(1, 0);
+            string invalidPkHex = "0x99da9559e15e913ee9ab2e53e3dfad575da3349be1125bb922e33494f49882!";
+            var ex = Assert.Throws<ArgumentException>(() => new PrivateKey(invalidPkHex));
+            Assert.AreEqual("Invalid key: \nParameter name: privateKey", ex.Message);
         }
 
         [Test]
@@ -63,14 +71,21 @@ namespace Sui.Tests.Cryptography
             string publicKey = "Gy9JCW4+Xb0Pz6nAwM2S2as7IVRLNNXdSmXZi4eLmSI=";
             PrivateKey pk = new PrivateKey(pkBase64);
 
-            Assert.AreEqual(1, 0, pk.PublicKey().ToBase64());
-            //Assert.AreEqual(pkBase64, pk.KeyBase64);
+            //Assert.AreEqual(1, 0, pk.PublicKey().ToBase64());
+            Assert.AreEqual(pkBase64, pk.KeyBase64);
         }
 
         [Test]
         public void PrivateKeyFromBase64StringInvalid()
         {
-            Assert.AreEqual(1, 0);
+            //string pkHex = "0x99da9559e15e913ee9ab2e53e3dfad575da33b49be1125bb922e33494f498828";
+            string pkBase64Invalid = "mdqVWeFekT7pqy5T49+tV12jO0m+ESW7kSU9JiCg=";
+            //byte[] pkBytes = { 153, 218, 149, 89, 225, 94, 145, 62, 233, 171, 46, 83, 227, 223, 173, 87, 93, 163, 59, 73, 190, 17, 37, 187, 146, 46, 51, 73, 79, 73, 136, 40 };
+            //string publicKey = "Gy9JCW4+Xb0Pz6nAwM2S2as7IVRLNNXdSmXZi4eLmSI=";
+            //PrivateKey pk = new PrivateKey(pkBase64);
+
+            var ex = Assert.Throws<ArgumentException>(() => new PrivateKey(pkBase64Invalid));
+            Assert.AreEqual("Invalid key: \nParameter name: privateKey", ex.Message);
         }
 
         [Test]
