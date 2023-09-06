bda2614b1598c72f4893bd212d4f6779071dad12
Irvin <irvsteve@gmail.com>
2023-09-06 00:49:50 -0400

    Add template for AccountFactory

---
+++ Assets/Sui-Unity-SDK/Code/Sui.Accounts/AccountFactory.cs
@@ -0,0 +1,25 @@
+
+using System;
+using Sui.Cryptography;
+using static Sui.Cryptography.SignatureUtils;
+
+namespace Sui.Accounts
+{
+    public class AccountFactory
+    {
+        public static Account CreateAccount(PrivateKeyBase privateKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Account CreateAccount(SignatureScheme signatureScheme = SignatureScheme.ED25519)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Account CreateAccount(byte[] privateKey, SignatureScheme signatureScheme = SignatureScheme.ED25519)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
