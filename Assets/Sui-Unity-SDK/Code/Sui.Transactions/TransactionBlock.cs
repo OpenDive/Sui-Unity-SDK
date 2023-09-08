5d2ffd88c78f1feb492e326626e27bfe00cbbc4d
MarcoDotIO <Marcus.Arnett10@gmail.com>
2023-09-08 19:33:51 -0400

    Add comments to TransactionBlock.cs

--- Assets/Sui-Unity-SDK/Code/Sui.Transactions/TransactionBlock.cs
+++ Assets/Sui-Unity-SDK/Code/Sui.Transactions/TransactionBlock.cs
@@ -1,7 +1,46 @@
-
 namespace Sui.Transactions
 {
     public class TransactionBlock
     {
+        /// VARIABLES
+        // blockData: TransactionBlockDataBuilder
+        // defaultOfflineLimits: [String: Int]
+
+        // INITIALIZER
+        // init(blockData: TransactionBlockDataBuilder?)
+
+        /// SENDER FUNCTIONS
+        // setSender(sender: String)
+        // setSenderIfNotSet(sender: String) throws
+        // setExpiration(expiration: TransactionExpiration)
+
+        /// GAS FUNCTIONS
+        // setGasPrice(price: Int)
+        // setGasBudget(budget: Int)
+        // setGasOwner(owner: String)
+        // setGasPayment(payments: [SuiObjectRef])
+        // gas: TransactionArgument
+
+        /// INPUT FUNCTIONS
+        // input(type: ValueType, value: SuiJsonValue?) throws -> TransactionBlockInput
+        // object(value: String) throws -> TransactionBlockInput
+        // object(value: ObjectArg) throws -> TransactionBlockInput
+        // objectRef(objectArg: ObjectArg) throws -> [TransactionBlockInput]
+        // shredObjectRef(sharedObjectRef: SharedObjectRef) throws -> [TransactionBlockInput]
+        // pure(value: SuiJsonValue) throws -> TransactionArgument
+        // add(transaction: SuiTransaction) throws -> TransactionArgument
+
+        /// TRANSACTION FUNCTIONS
+        // splitCoin(coin: TransactionArgument, amounts: [TransactionBlockInput]) throws -> TransactionArgument
+        // mergeCoin(destination: TransactionBlockInput, sources: [TransactionBlockInput]) throws -> TransactionArgument
+        // publish(modules: [Data], dependecies: [String]) throws -> TransactionArgument
+        // publish(modules: [String], dependencies: [String]) throws -> TransactionArgument
+        // upgrade(modules: [Data], dependencies: [String], packageId: String, ticket: TransactionArgument) throws -> TransactionArgument
+        // moveCall(target: String, arguments: [TransactionArgument]? = nil, typeArguments: [String]? = nil) throws -> TransactionArgument
+        // transferObject(objects: [TransactionArgument], address: String) throws -> TransactionArgument
+        // makeMoveVec(type: String? = nil, objects: [TransactionBlockInput]) throws -> TransactionArgument
+
+        /// PREPARRING TRANSACTION FUNCTIONS
+        // getConfig(key: LimitKey, buildOptions: BuildOptions) throws -> Int
     }
 }
