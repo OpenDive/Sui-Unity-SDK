72c6aa50296b242d750bb3d25fe7e5d83a39df88
Irvin <irvsteve@gmail.com>
2023-09-09 00:28:00 -0400

    Add initial implementation of transactions

---
+++ Assets/Sui-Unity-SDK/Code/Sui.Transactions/Builder/TransactionObjects/MoveCallTransaction.cs
@@ -0,0 +1,80 @@
+using System;
+using OpenDive.BCS;
+using Sui.Accounts;
+
+namespace Sui.Transactions.Builder.TransactionObject
+{
+    public class MoveCallTransaction : TransactionBase, ISerializable
+    {
+        /// <summary>
+        /// The module id that contains the target function
+        /// Represents the following:
+        /// address_hex::module::name
+        ///
+        /// new ModuleId(AccountAddress.FromHex("0x4"), "aptos_token"), "burn"
+        /// </summary>
+        public ModuleId ModuleId { get; private set; }
+
+        /// <summary>
+        /// The target function name
+        /// </summary>
+        public string Function { get; private set; }
+
+        /// <summary>
+        /// The target
+        /// </summary>
+        public string Target { get; private set; }
+
+        /// <summary>
+        ///
+        /// new TagSequence(new ISerializableTag[] { new StructTag(AccountAddress.FromHex("0x4"), "token", "Token", new ISerializableTag[0]) })
+        /// </summary>
+        public TagSequence TypeArguments { get; private set; }
+
+        /// <summary>
+        /// The sequence of arguments
+        /// </summary>
+        public Sequence Arguments { get; private set; }
+
+
+        public MoveCallTransaction(ModuleId moduleId, string function, ISerializableTag[] typeArguments, ISerializable[] arguments)
+        {
+            ModuleId = moduleId;
+            Function = function;
+            Target = ModuleId.ToString() + "::" + Function;
+            TypeArguments = new TagSequence(typeArguments);
+            Arguments = new Sequence(arguments);
+        }
+
+        public override string EncodeTransaction()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Serialize(Serialization serializer)
+        {
+            new BString(Target).Serialize(serializer);
+            TypeArguments.Serialize(serializer);
+            Arguments.Serialize(serializer);
+        }
+
+        public static ISerializable Deserialize(Deserialization deserializer)
+        {
+            BString target = BString.Deserialize(deserializer);
+            TagSequence typeArguments = TagSequence.Deserialize(deserializer);
+            Sequence arguments = Sequence.Deserialize(deserializer);
+
+            string targetStr = (string)target.GetValue();
+            string[] split = targetStr.Split("::");
+            ModuleId moduleId = new ModuleId(AccountAddress.FromHex(split[0]), split[1]);
+            string function = split[2];
+
+            return new MoveCallTransaction(
+                moduleId,
+                function,
+                (ISerializableTag[])typeArguments.GetValue(),
+                (ISerializable[])arguments.GetValue()
+            );
+        }
+    }
+}
---
+++ Assets/Sui-Unity-SDK/Code/Sui.Transactions/Builder/TransactionObjects/TransactionBase.cs
@@ -0,0 +1,7 @@
+namespace Sui.Transactions.Builder.TransactionObject
+{
+    public abstract class TransactionBase
+    {
+        public abstract string EncodeTransaction();
+    }
+}
