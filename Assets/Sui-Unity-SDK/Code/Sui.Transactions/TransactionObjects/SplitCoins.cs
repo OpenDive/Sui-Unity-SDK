24799e23b0e8eedabef1772749fccf3be6decae5
Irvin <irvsteve@gmail.com>
2023-09-11 13:56:21 -0400

    Update interface for SplitCoins object

--- Assets/Sui-Unity-SDK/Code/Sui.Transactions/TransactionObjects/SplitCoins.cs
+++ Assets/Sui-Unity-SDK/Code/Sui.Transactions/TransactionObjects/SplitCoins.cs
@@ -1,6 +1,12 @@
+using OpenDive.BCS;
+
 namespace Sui.Transactions.Objects
 {
     public class SplitCoins : ITransactionObject
     {
+        public void Serialize(Serialization serializer)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
