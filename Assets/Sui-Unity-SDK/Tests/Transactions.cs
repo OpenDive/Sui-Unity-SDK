bfd84a02b3ce362dc4c09db320d0e791f4e5de41
Irvin <irvsteve@gmail.com>
2023-09-13 05:36:59 -0400

    Update transaction test

--- Assets/Sui-Unity-SDK/Tests/Transactions.cs
+++ Assets/Sui-Unity-SDK/Tests/Transactions.cs
@@ -13,10 +13,55 @@ namespace Sui.Tests
 {
     public class Transactions : MonoBehaviour
     {
+        string test     = "0x0000000000000000000000000000000000000000000000000000000000000BAD";
+        string objectId = "1000000000000000000000000000000000000000000000000000000000000000";
+        int version     = int.Parse("10000");
+        string digest   = "1Bhh3pU9gLXZhoVxkr5wyg9sX6";
+        string suiAddressHex = "0x0000000000000000000000000000000000000000000000000000000000000002";
+
+        [Test]
+        public void TransactionDataSerializationSingleInput()
+        {
+            AccountAddress suiAddress = AccountAddress.FromHex(suiAddressHex);
+
+            // Inputs
+            SuiObjectRef paymentRef = new SuiObjectRef(objectId, version, digest);
+            ICallArg[] inputs = new ICallArg[] { new ObjectCallArg(paymentRef) };
+
+            MoveCall moveCallTransaction = new MoveCall(
+                new SuiStructTag(suiAddress, "display", "new", new ISerializableTag[0]), // TODO: THIS IS A NORMALIZED STRUCT
+                new ISerializableTag[] { new StructTag(suiAddress, "capy", "Capy", new ISerializableTag[0]) },
+                new ITransactionArgument[] { new Sui.Transactions.Types.Arguments.Input(0) }
+            );
+            ITransaction[] transactions = new[] { moveCallTransaction };
+
+            // Transactions
+            TransactionData transactionData = new TransactionData(
+                AccountAddress.FromHex(test),
+                new TransactionExpiration(),
+                new GasConfig(
+                    "1000000",
+                    "1",
+                    new SuiObjectRef[] { paymentRef },
+                    suiAddress
+                ),
+                new ProgrammableTransaction(inputs, transactions)
+            );
+
+            Serialization serializer = new Serialization();
+            transactionData.Serialize(serializer);
+            byte[] actual = serializer.GetBytes();
+            byte[] expected = new byte[] { 0, 0, 1, 1, 0, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 39, 0, 0, 0, 0, 0, 0, 20, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 7, 100, 105, 115, 112, 108, 97, 121, 3, 110, 101, 119, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 99, 97, 112, 121, 4, 67, 97, 112, 121, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 173, 1, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 39, 0, 0, 0, 0, 0, 0, 20, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 64, 66, 15, 0, 0, 0, 0, 0, 0 };
+
+            Assert.AreEqual(expected, actual,
+                "ACTUAL LENGHT: " + actual.Length + "\n"
+                + "EXPECTED LENGTH: " + expected.Length + "\n" + actual.ByteArrayToString());
+        }
+
         [Test]
         public void TransactionDataSerialization()
         {
-            string test = "0x0000000000000000000000000000000000000000000000000000000000000BAD";
+            //string test = "0x0000000000000000000000000000000000000000000000000000000000000BAD";
 
             AccountAddress sender = AccountAddress.FromHex(test);
             TransactionExpiration expiration = new TransactionExpiration();
