c08dc38995ea8b44a5435c4845355461ea3c5559
Irvin <irvsteve@gmail.com>
2023-09-12 22:29:54 -0400

    Implement TransferObjects

--- Assets/Sui-Unity-SDK/Code/Sui.Transactions/Types/TransferObjects.cs
+++ Assets/Sui-Unity-SDK/Code/Sui.Transactions/Types/TransferObjects.cs
@@ -1,5 +1,7 @@
+using System;
 using OpenDive.BCS;
 using Sui.Accounts;
+using Sui.BCS;
 using Sui.Transactions.Builder;
 
 namespace Sui.Transactions.Types
@@ -21,13 +23,22 @@ namespace Sui.Transactions.Types
         /// Creates TransferObjects transaction.
         /// </summary>
         /// <param name="objects">Objects that we are transferring.</param>
-        /// <param name="address">The recepient address nested withing the TransactionBlockInput.
+        /// <param name="address">The recepient address (AccountAddress) nested within the TransactionBlockInput.
         /// This will be what Sui refers to as "Pure". TODO: Check if we need to simply encode it as byte array.
         /// </param>
         public TransferObjects(ITransactionType[] objects, TransactionBlockInput address)
         {
             Objects = objects;
-            Address = address;
+
+            Type callArgType = address.Value.GetType();
+            if (callArgType == typeof(PureCallArg)
+                && ((PureCallArg)address.Value).Value.GetType() == typeof(AccountAddress)) {
+                Address = address;
+            }
+            else
+            {
+                throw new ArgumentException("TransactionBlockInput must be of Pure type and and AccountAddress");
+            }
         }
 
         public void Serialize(Serialization serializer)
