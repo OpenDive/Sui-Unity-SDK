9f4c95999883407fb7719f1a5710c9300ec4d370
Marcus Arnett <marcodotio@marcuss-mbp-4.lan>
2024-02-19 15:59:34 -0500

    Implement Publish BCS

--- Assets/Sui-Unity-SDK/Code/Sui.Transactions/Types/Publish.cs
+++ Assets/Sui-Unity-SDK/Code/Sui.Transactions/Types/Publish.cs
@@ -1,5 +1,8 @@
 using System;
 using OpenDive.BCS;
+using Sui.Accounts;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Sui.Transactions.Types
 {
@@ -9,15 +12,39 @@ namespace Sui.Transactions.Types
     public class Publish : ITransaction
     {
         public Kind Kind => Kind.Publish;
+        public byte[][] Modules;
+        public AccountAddress[] Dependencies;
 
-        public Publish(byte[][] modules, string[] dependencies) // dependencies are ObjectId
+        public Publish(byte[][] modules, AccountAddress[] dependencies) // dependencies are ObjectId
         {
-            throw new NotImplementedException();
+            this.Modules = modules;
+            this.Dependencies = dependencies;
         }
 
         public void Serialize(Serialization serializer)
         {
-            throw new System.NotImplementedException();
+            serializer.SerializeU32AsUleb128(4);
+            serializer.SerializeU64((ulong)Modules.Length);
+            foreach(byte[] module in Modules)
+            {
+                serializer.Serialize(module);
+            }
+            serializer.Serialize(Dependencies);
+        }
+
+        public static Publish Deserialize(Deserialization deserializer)
+        {
+            deserializer.DeserializeUleb128();
+            List<byte[]> modules = new List<byte[]>();
+            ulong length = deserializer.DeserializeU64();
+            for(ulong i = 0; i < length; ++i)
+            {
+                modules.Add(deserializer.ToBytes());
+            }
+            return new Publish(
+                modules.ToArray(),
+                deserializer.DeserializeSequence(typeof(AccountAddress)).Cast<AccountAddress>().ToArray()
+            );
         }
     }
 }
