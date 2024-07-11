2eba198eeb4ca10100463151f741fd976339245d
MarcoDotIO <Marcus.Arnett10@gmail.com>
2024-07-11 10:30:37 -0400

    Implement ProtocolConfigTest

---
+++ Assets/Sui-Unity-SDK/Tests/ProtocolConfigTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine.TestTools;
+using UnityEngine;
+using NUnit.Framework;
+using Sui.Rpc;
+using Sui.Rpc.Models;
+
+namespace Sui.Tests
+{
+    public class ProtocolConfigTest
+    {
+        TestToolbox Toolbox;
+
+        [UnitySetUp]
+        public IEnumerator SetUp()
+        {
+            this.Toolbox = new TestToolbox();
+            yield return this.Toolbox.Setup();
+        }
+
+        [UnityTest]
+        public IEnumerator ProtocolConfigFetchTest()
+        {
+            Task<RpcResult<ProtocolConfig>> config_task = this.Toolbox.Client.GetProtocolConfigAsync();
+            yield return new WaitUntil(() => config_task.IsCompleted);
+
+            Assert.IsTrue(config_task.Result.Result.ProtocolVersion != "");
+        }
+    }
+}
