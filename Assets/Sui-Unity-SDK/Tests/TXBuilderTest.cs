93755bc6016af71b69513d7ed8dcedcc84493593
MarcoDotIO <Marcus.Arnett10@gmail.com>
2024-07-16 00:30:36 -0400

    Implement TXBuilderTest

--- Assets/Sui-Unity-SDK/Tests/TXBuilderTest.cs
+++ Assets/Sui-Unity-SDK/Tests/TXBuilderTest.cs
@@ -1,27 +1,16 @@
 ﻿using NUnit.Framework;
 using OpenDive.BCS;
 using Sui.Accounts;
-using Sui.Transactions.Builder;
-using Sui.Transactions.Kinds;
 using UnityEngine;
-using Sui.Utilities;
-using Sui.Transactions.Types;
 using Sui.Transactions.Types.Arguments;
-using Sui.Types;
 using Sui.Transactions;
 using Sui.Rpc;
 using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
-using System;
-using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Sui.Clients;
 using UnityEngine.TestTools;
 using Sui.Rpc.Models;
-using Newtonsoft.Json;
-using UnityEditor.VersionControl;
 
 namespace Sui.Tests
 {
@@ -176,6 +165,108 @@ namespace Sui.Tests
                 new SuiTransactionArgument[] { tx_block.AddPure(new U64(1)) }
             );
             tx_block.AddTransferObjectsTx(coin.ToArray(), this.Toolbox.DefaultRecipient);
+
+            yield return this.ValidateTransaction
+            (
+                this.Toolbox.Client,
+                this.Toolbox.Account,
+                tx_block
+            );
+        }
+
+        [UnityTest]
+        public IEnumerator TransferObjectGasCoinTest()
+        {
+            yield return this.Toolbox.Setup();
+
+            Transactions.TransactionBlock tx_block = new Transactions.TransactionBlock();
+            tx_block.AddTransferObjectsTx
+            (
+                new SuiTransactionArgument[] { tx_block.gas },
+                this.Toolbox.DefaultRecipient
+            );
+
+            yield return this.ValidateTransaction
+            (
+                this.Toolbox.Client,
+                this.Toolbox.Account,
+                tx_block
+            );
+        }
+
+        [UnityTest]
+        public IEnumerator TransferObjectsTest()
+        {
+            yield return this.Toolbox.Setup();
+
+            Task<RpcResult<CoinPage>> coins_task = this.Toolbox.GetCoins();
+            yield return new WaitUntil(() => coins_task.IsCompleted);
+
+            CoinDetails coin_0 = coins_task.Result.Result.Data[0];
+
+            Transactions.TransactionBlock tx_block = new Transactions.TransactionBlock();
+            tx_block.AddTransferObjectsTx
+            (
+                new SuiTransactionArgument[] { tx_block.AddObjectInput(coin_0.CoinObjectId) },
+                this.Toolbox.DefaultRecipient
+            );
+
+            yield return this.ValidateTransaction
+            (
+                this.Toolbox.Client,
+                this.Toolbox.Account,
+                tx_block
+            );
+        }
+
+        [UnityTest]
+        public IEnumerator MoveSharedMutableImmutableReferencesTest()
+        {
+            yield return this.Toolbox.Setup();
+
+            Transactions.TransactionBlock tx_block = new Transactions.TransactionBlock();
+            tx_block.AddMoveCallTx
+            (
+                new SuiMoveNormalizedStructType(SuiStructTag.FromStr($"{this.PackageID}::serializer_tests::value"), new SuiMoveNormalizedType[] { }),
+                new SerializableTypeTag[] { },
+                new SuiTransactionArgument[]
+                {
+                        tx_block.AddObjectInput(this.SharedObjectID)
+                }
+            );
+            tx_block.AddMoveCallTx
+            (
+                new SuiMoveNormalizedStructType(SuiStructTag.FromStr($"{this.PackageID}::serializer_tests::set_value"), new SuiMoveNormalizedType[] { }),
+                new SerializableTypeTag[] { },
+                new SuiTransactionArgument[]
+                {
+                        tx_block.AddObjectInput(this.SharedObjectID)
+                }
+            );
+
+            yield return this.ValidateTransaction
+            (
+                this.Toolbox.Client,
+                this.Toolbox.Account,
+                tx_block
+            );
+        }
+
+        [UnityTest]
+        public IEnumerator ImmutableClockFunctionTest()
+        {
+            yield return this.Toolbox.Setup();
+
+            Transactions.TransactionBlock tx_block = new Transactions.TransactionBlock();
+            tx_block.AddMoveCallTx
+            (
+                new SuiMoveNormalizedStructType(SuiStructTag.FromStr($"{this.PackageID}::serializer_tests::use_clock"), new SuiMoveNormalizedType[] { }),
+                new SerializableTypeTag[] { },
+                new SuiTransactionArgument[]
+                {
+                        tx_block.AddObjectInput(this.SuiClockObjectID)
+                }
+            );
 
             yield return this.ValidateTransaction
             (
